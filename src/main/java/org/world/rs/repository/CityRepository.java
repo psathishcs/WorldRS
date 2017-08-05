41fc69010ab0278e60b134d31541b4bd63b2325a
SathishKumarParthasarathy <p.sathish.cs@gmail.com>
2017-08-05 10:52:48 -0500

    fix

--- src/main/java/org/world/rs/repository/CityRepository.java
+++ src/main/java/org/world/rs/repository/CityRepository.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.world.rs.entity.City;
 
+@Repository
+@Transactional
 public class CityRepository implements WorldRepository{
 	@Autowired
 	private SessionFactory _sessionFactory;
