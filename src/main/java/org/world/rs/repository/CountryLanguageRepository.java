bcc6461763c5cf332005fa8c61020516c86276bc
SathishKumarParthasarathy <p.sathish.cs@gmail.com>
2017-08-05 16:26:19 -0500

    Fix

--- src/main/java/org/world/rs/repository/CountryLanguageRepository.java
+++ src/main/java/org/world/rs/repository/CountryLanguageRepository.java
@@ -1,5 +1,26 @@
 package org.world.rs.repository;
 
-public class CountryLanguageRepository {
+import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.world.rs.entity.CountryLanguage;
+
+@Repository
+@Transactional
+public class CountryLanguageRepository {
+	@Autowired
+	private SessionFactory _sessionFactory;
+	
+	private Session getSession(){
+		return _sessionFactory.getCurrentSession();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<CountryLanguage> getAll() {
+		return (List<CountryLanguage>) getSession().createQuery("from CountryLanguage").list();
+	}
 }
